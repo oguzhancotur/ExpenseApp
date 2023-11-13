67f14a4a16abbeffcda239117b0a9abf26dcae43
Oguzhan Cotur <oguzhancotur55@gmail.com>
2023-11-13 22:13:35 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: ExpenseApp(),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
